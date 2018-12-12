705693d75360dd3f0dfac26975d3da18f0e1acd5
tankeryang <youngzyang@outlook.com>
2018-12-12 22:34:04 +0800

    mod

--- presto-etl/presto-etl.py
+++ presto-etl/presto-etl.py
@@ -376,7 +376,7 @@ class PrestoETL():
         
         self.get_sql_file()
         self.get_placeholder_config(presto_engine)
-
+        print(self.__sql_file.keys())
         if len(self.__placeholder_config) != 0:
             self.get_placeholder_group()
 
