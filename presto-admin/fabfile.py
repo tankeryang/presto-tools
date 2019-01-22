7b5e20c86a3ca71fae4edb5bb2e4e1b84cc1e298
tankeryang <youngzyang@outlook.com>
2019-01-22 19:32:31 +0800

    update: presto-admin test

---
+++ presto-admin/fabfile.py
@@ -0,0 +1,36 @@
+import configparser
+from fabric import Connection, SerialGroup
+from invoke import task
+
+# load config
+config = configparser.ConfigParser()
+config.read('config.ini')
+
+# set coordinato
+coordinator_connections = []
+coordinator_user = config['COORDINATOR'].pop('user')
+coordinator_password = config['COORDINATOR'].pop('password')
+coordinator_catalog_path = config['COORDINATOR'].pop('catalog_path')
+for host_key in config['COORDINATOR'].keys():
+    host = config['COORDINATOR'][host_key]
+    conn = Connection(host=host, user=coordinator_user, connect_kwargs={'password': coordinator_password})
+    coordinator_connections.append(conn)
+coordinator_group = SerialGroup.from_connections(coordinator_connections)
+
+# set worker
+worker_connections = []
+worker_user = config['WORKER'].pop('user')
+worker_password = config['WORKER'].pop('password')
+worker_catalog_path = config['WORKER'].pop('catalog_path')
+for host_key in config['WORKER'].keys():
+    host = config['WORKER'][host_key]
+    conn = Connection(host=host, user=worker_user, connect_kwargs={'password': worker_password})
+    worker_connections.append(conn)
+worker_group = SerialGroup.from_connections(worker_connections)
+
+
+@task
+def uname(c):
+    coordinator_group.run('uname -s')
+    worker_group.run('uname -s')
+    c.run('uname -s')
--- presto-admin/presto-admin.py
+++ presto-admin/presto-admin.py
@@ -1,1 +1,90 @@
-from fabric import Connection
+import os
+import sys
+import argparse
+import logging
+import textwrap
+import configparser
+
+
+class PrestoAdmin():
+    """
+    Presto 管理工具
+    """
+
+    def __init__(self):
+        """
+        初始化时将参数通过 self.__set_args() 绑定到 self.__args 变量上
+        """
+        self.__args = self.__set_args()
+        self.__config = configparser.ConfigParser()
+
+
+    def __set_args(self):
+        """
+        设置参数选项
+        """
+        parser = argparse.ArgumentParser(prog="python3 presto-admin.py", description="This is a python manage script")
+
+        parser.add_argument('--usage', action='store_true', dest='usage', default=False, help="show usage")
+        parser.add_argument(
+            '--reload-catalog', action='store_true', dest='reload_catalog', default=False, help="reload catalog"
+        )
+
+        # test
+        parser.add_argument('--uname', action='store_true', dest='uname', default=False, help="uname test")
+
+        # # set presto coordinator hosts, password and catalog path
+        # parser.add_argument(
+        #     '--coordinator-hosts', action='store', dest='coordinator_hosts', nargs='*',
+        #     help="set presto coordinator hosts"
+        # )
+        # parser.add_argument(
+        #     '--coordinator-password', action='store', dest='coordinator_password',
+        #     help="set presto coordinator password"
+        # )
+        # parser.add_argument(
+        #     '--coordinator-catalog-path', action='store', dest='coordinator_catalog_path',
+        #     help="set coordinator presto catalog path (e.g. /etc/ecm/presto-conf/catalog)"
+        # )
+
+        # # set presto worker hosts and password
+        # parser.add_argument(
+        #     '--worker-hosts', action='store', dest='worker_hosts', nargs='*',
+        #     help="set presto worker hosts"
+        # )
+        # parser.add_argument(
+        #     '--worker-password', action='store', dest='worker_password',
+        #     help="set presto worker password"
+        # )
+        # parser.add_argument(
+        #     '--worker-catalog-path', action='store', dest='worker_catalog_path',
+        #     help="set worker presto catalog path (e.g. /etc/ecm/presto-conf/catalog)"
+        # )
+
+        args = parser.parse_args()
+
+        # set args_dict
+        args_key = list(map(lambda kv: kv[0], args._get_kwargs()))
+        args_value = list(map(lambda kv: kv[1], args._get_kwargs()))
+
+        self.__args_dict = dict(zip(args_key, args_value))
+
+        return args
+
+
+    def reload_catalog(self):
+        """
+        reload catalog file
+        """
+        if self.__args.reload_catalog is True:
+            os.system('fab reload_catalog')
+
+
+    def uname(self):
+        if self.__args.uname is True:
+            os.system('fab uname')
+
+
+if __name__ == '__main__':
+    presto_admin = PrestoAdmin()
+    presto_admin.uname()
